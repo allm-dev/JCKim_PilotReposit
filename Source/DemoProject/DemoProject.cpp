722472e85290ce9b5d083f1b049e526308b0c5ef
Jongcohol <86995898+Jongcohol@users.noreply.github.com>
2021-07-13 09:31:27 +0900

    Delete Source directory

--- Source/DemoProject/DemoProject.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "DemoProject.h"
-#include "Modules/ModuleManager.h"
-
-DEFINE_LOG_CATEGORY(DemoProject);
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DemoProject, "DemoProject" );
